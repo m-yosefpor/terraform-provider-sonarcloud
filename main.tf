09fa66d1faef4e03cbb6b5d6865c5dda3895b25e
Matt Yosefpor <47300215+m-yosefpor@users.noreply.github.com>
2025-01-06 14:47:18 +0100

    variable list for projects
    Signed-off-by: Matt Yosefpor <47300215+m-yosefpor@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -8,16 +8,18 @@ terraform {
 }
 
 provider "sonarcloud" {
-  token    = var.sonarcloud_token
+  token        = var.sonarcloud_token
   organization = var.organization
 }
 
-resource "sonarcloud_project" "example" {
-  project_key  = "bitvavo_crypto-data-aggregation-service"
-  name         = "crypto-data-aggregation-service"
-  visibility   = "private"
-  organization = var.organization
-  new_code_definition_type = "days"
+resource "sonarcloud_project" "projects" {
+  for_each = toset(var.projects)
+
+  project_key               = "${var.organization}_${each.key}"
+  name                      = each.key
+  visibility                = "private"
+  organization              = var.organization
+  new_code_definition_type  = "days"
   new_code_definition_value = "30"
 }
 
@@ -31,9 +33,11 @@ resource "sonarcloud_project" "example" {
 # }
 
 
-resource "sonarcloud_qualitygates_select" "example" {
-  project_key     = sonarcloud_project.example.project_key
-  organization = var.organization
+resource "sonarcloud_qualitygates_select" "projects" {
+  for_each = sonarcloud_project.projects
+
+  project_key     = each.value.project_key
+  organization    = var.organization
   quality_gate_id = 117724
 }
 
@@ -49,4 +53,10 @@ variable "sonarcloud_token" {
 
 variable "organization" {
   default = "bitvavo"
+}
+
+
+variable "projects" {
+  type    = list(string)
+  default = []
 }
